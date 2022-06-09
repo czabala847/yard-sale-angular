54c8f11e84516a4b5e753a3e80e2fac826bb5c26
Carlos Zabala <czabala847@gmail.com>
2022-06-09 13:54:44 -0500

    Añadiendo estado global

--- src/app/components/nav/nav.component.ts
+++ src/app/components/nav/nav.component.ts
@@ -1,16 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { StoreService } from 'src/app/services/store.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-nav',
   templateUrl: './nav.component.html',
   styleUrls: ['./nav.component.scss'],
 })
-export class NavComponent implements OnInit {
+export class NavComponent implements OnInit, OnDestroy {
   activeMenu: boolean = false;
 
-  constructor() {}
+  count: number = 0;
+  private countSubs!: Subscription;
 
-  ngOnInit(): void {}
+  constructor(private store: StoreService) {}
+
+  ngOnInit(): void {
+    //suscribiéndose al estado global que ofrece el servicio StoreService
+    this.store.storeCart$.subscribe((products) => {
+      this.count = products.length;
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.countSubs.unsubscribe();
+  }
 
   toggleMenu() {
     this.activeMenu = !this.activeMenu;
--- src/app/services/store.service.ts
+++ src/app/services/store.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
 import { ProductInterface } from '../models/product.model';
 
 @Injectable({
@@ -6,11 +7,21 @@ import { ProductInterface } from '../models/product.model';
 })
 export class StoreService {
   private cart: ProductInterface[] = [];
+  private storeCart = new BehaviorSubject<ProductInterface[]>([]);
+  private storeQuantityCart = new BehaviorSubject<number>(0);
+
+  //definir observable
+  storeCart$ = this.storeCart.asObservable();
+  storeQuantityCart$ = this.storeQuantityCart.asObservable();
 
   constructor() {}
 
   addProduct(product: ProductInterface) {
     this.cart.push(product);
+
+    //Todos los que esten suscritos, recibiran una notificación
+    this.storeCart.next(this.cart);
+    this.storeQuantityCart.next(this.getQuantityCart());
   }
 
   getCart() {
